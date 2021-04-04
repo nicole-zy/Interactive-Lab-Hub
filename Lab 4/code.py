923049d78012c16a6b7a9a02d7ded19bd48ad5e1
nicole-zy <78866283+nicole-zy@users.noreply.github.com>
2021-04-03 21:27:09 -0400

    Update code.py

--- Lab 4/code.py
+++ Lab 4/code.py
@@ -4,6 +4,81 @@ import busio
 import random
 
 import adafruit_mpr121
+from time import strftime, sleep
+import subprocess
+import digitalio
+import board
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
+import os
+
+
+# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 64000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the ST7789 display:
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+height = disp.width  # we swap height/width to rotate it to landscape!
+width = disp.height
+image = Image.new("RGB", (width, height))
+rotation = 90
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.image(image, rotation)
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height - padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the
+# same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+
+# Turn on the backlight
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+
+    # Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=0)
+y = top
+a = "Welcome to the Dance Dance Revolution Game\n The game begins now"
+
+draw.text((x, y), a, font=font, fill="#0000FF")
+
 
 i2c = busio.I2C(board.SCL, board.SDA)
 
@@ -20,19 +95,19 @@ while True:
    
     direction = random.randint(1, 2)
     if direction == 1:
-        print("left")
+        draw.text((x, y), "left", font=font, fill="#0000FF")
         time.sleep(0.1)
         while not (left.value or right.value):
             time.sleep(0.01)
         if right.value:
-            print("Error - Game lost")
+            draw.text((x, y), "Game over", font=font, fill="#0000FF")
             break
     else:
-        print("right")
+        pdraw.text((x, y), "Right", font=font, fill="#0000FF")
         time.sleep(0.1)
         while not (left.value or right.value):
             time.sleep(0.01)
         if left.value:
-            print("Error - Game lost")
+            draw.text((x, y), "Game over", font=font, fill="#0000FF")
             break
 
