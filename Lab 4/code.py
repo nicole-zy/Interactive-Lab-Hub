95cf2de7cc9edda05227a5b4a0c5846ab2bd72b9
nicole-zy <78866283+nicole-zy@users.noreply.github.com>
2021-04-04 14:10:58 -0400

    Update code.py

--- Lab 4/code.py
+++ Lab 4/code.py
@@ -86,14 +86,17 @@ buttonB.switch_to_input()
     # Draw a black filled box to clear the image.
 draw.rectangle((0, 0, width, height), outline=0, fill=0)
 y = top
-a = "Welcome to \n Dance Dance \n Revolution Game\n The game begins now!"
-
-
-
+a = "Welcome to \n Dance Dance \nRevolution Game"
 draw.text((x, y), a, font=font, fill="#FFFFFF")
 y += font.getsize(a)[1]
 disp.image(image, rotation)
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.image(image, rotation)
 handle_speak("Welcome to Dance Dance revolution game.")
+a = "The game begins now!"
+draw.text((x, y), a, font=font, fill="#FFFFFF")
+y += font.getsize(a)[1]
+disp.image(image, rotation)
 handle_speak("The game begins now.")
 time.sleep(2)
 draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
