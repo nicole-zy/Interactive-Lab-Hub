1437be5d1fc9a6719400645fe5b968b79bd62a62
nicole-zy <78866283+nicole-zy@users.noreply.github.com>
2021-05-09 22:02:13 -0400

    Update piano.py

--- Lab 6/piano.py
+++ Lab 6/piano.py
@@ -32,7 +32,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 # to the right size for your display!
 # oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
 # image = Image.new("1", (oled.width, oled.height))
-draw = ImageDraw.Draw(image)
+# draw = ImageDraw.Draw(image)
 
 music_script = deque()
 
